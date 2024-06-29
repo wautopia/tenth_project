bb10bc231b05cc8c0fb9d0732fc87d3311ccbb1a
wautopia <gameprogrammer55@gmail.com>
2024-06-30 01:30:05 +0900

    文字化け修正

--- src/Model/m_text.js
+++ src/Model/m_text.js
@@ -8,27 +8,27 @@ function Text() {
             <div className="Text position-absolute z-2 text-center w-100" style={{ marginTop: '140px', marginBottom: '140px'}}>
                 <img src={Text1} className="text1" alt="" />
                 <p className="text2" style={{ paddingTop: '80px' }}>
-                    ���Ȃ������̐S��䂫���ė����Ȃ��A�Ƃ��鏗���̐l��杁B<br />
-                    �ޏ��̖��́u���@�C�I���b�g�E�G���@�[�K�[�f���v�����B
+                    今なお多くの心を惹きつけて離さない、とある女性の人生譚。<br />
+                    彼女の名は「ヴァイオレット・エヴァーガーデン」──。
                 </p>
                 <p className="text3" style={{ paddingTop: '40px' }}>
-                    �����O���烏�[���h�c�A�[��s���A���E�e�n�ŔM���̉Q������N�������B<br />
-                    �����I�ȕ���ƁA�@�ׂŃ_�C�i�~�b�N�ȉf���Řb��ƂȂ���TV�V���[�Y�A<br />
-                    �ϋq�����̐S���点�A��i���E�ւƗU�����I�[�P�X�g���R���T�[�g��o�āA<br />
-                    �u���@�C�I���b�g�E�G���@�[�K�[�f�� �O�` - �i���Ǝ�����L�l�` -�v����J�B<br />
-                    ���E���ő傫�Ȕ���������N�������B
+                    放送前からワールドツアーを行い、世界各地で熱狂の渦を巻き起こした。<br />
+                    感動的な物語と、繊細でダイナミックな映像で話題となったTVシリーズ、<br />
+                    観客たちの心を躍らせ、作品世界へと誘ったオーケストラコンサートを経て、<br />
+                    「ヴァイオレット・エヴァーガーデン 外伝 - 永遠と自動手記人形 -」を公開<br />
+                    世界中で大きな反響を巻き起こした。
                 </p>
-                <p className="text4" style={{ paddingTop: '40px' }}>�����āA���悢��t�B�i�[���ք����B</p>
-                <p className="text5 fs-5" style={{ paddingTop: '40px' }}>�u�����Ă�v��A�����͂킩��̂ł��B</p>
+                <p className="text4" style={{ paddingTop: '40px' }}>そして、いよいよフィナーレへ──。</p>
+                <p className="text5 fs-5" style={{ paddingTop: '40px' }}>「愛してる」も、少しはわかるのです。</p>
                 <p className="text6" style={{ paddingTop: '40px' }}>
-                    �V�������オ�������A���E���傫���ς���Ă������Ƃ��Ă��鍡�A<br />
-                    �ޏ��̖a���莆���A�ޏ��̑z�����A�ǂ��ɓ͂��̂��B
+                    新しい時代が到来し、世界が大きく変わっていこうとしている今、<br />
+                    彼女の紡ぐ手紙が、彼女の想いが、どこに届くのか。
                 </p>
-                <p className="text7 fs-5" style={{ paddingTop: '40px' }}>�s�ρz�Ły���Ձz�̈�����Ȃ��Ʉ����B</p>
+                <p className="text7 fs-5" style={{ paddingTop: '40px' }}>【不変】で【普遍】の愛をあなたに──。</p>
                 <img src={Text8} className="text8" style={{ paddingTop: '80px' }} alt="" />
                 <p className="text9 fs-4" style={{ paddingTop: '80px' }}>
-                    �w����� ���@�C�I���b�g�E�G���@�[�K�[�f���x<br />
-                    2020�N9��18�� �S��������J
+                    劇場版 ヴァイオレット・エヴァーガーデン』<br />
+                    2020年9月18日 全国劇場公開
                 </p>
             </div>
             <MBg/>
--- src/Model/m_visual_slider.js
+++ src/Model/m_visual_slider.js
@@ -9,7 +9,7 @@ import Visual02 from '../Media/m_visual02.jpg';
 
 function VisualSlider() {
     return (
-        <div className="VisualSlider" >
+        <div className="VisualSlider" style={{ width: "1506px" }}>
             <Swiper
                 speed={1300}
                 spaceBetween={30}
@@ -25,7 +25,7 @@ function VisualSlider() {
                 className="Swiper"
             >
                 <SwiperSlide>
-                    <div className="test" style={{ transition: "opacity .5s ease" }}>
+                    <div className="test" style={{ transition: "opacity .5s ease"}}>
                         <img src={Visual01} className="VisualImg" alt="" width="1506px" height="2103px" />
                     </div>
                 </SwiperSlide>
